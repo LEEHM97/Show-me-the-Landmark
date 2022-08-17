476ee70ae7ad9a2b64680d1b4115a21e1b0b8fe4
LEEHM97 <gmlkd2006@gmail.com>
2022-08-18 00:18:43 +0900

    feat: return class num to place name

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 import tensorflow as tf
+import os
 
 from tensorflow import keras 
 model = keras.models.load_model('./models/Vgg16-023-0.0384-0.9943.hdf5')
@@ -24,15 +25,37 @@ def hello():
 @app.route('/LandMark', methods = ['GET', 'POST'])
 def Cls_LandMark():
     if request.method == 'POST':
-        img = request.files['chooseFile']
-
-        print(type(img))
+        img = request.files['chooseFile'].read()
         
-        p_img = preprocessing_image()
+        img_bytes = np.fromstring(img, dtype = np.uint8)
+        img = cv2.imdecode(img_bytes, cv2.IMREAD_UNCHANGED)
+                        
+        p_img = preprocessing_image(img)
         prediction = model.predict(p_img)[0]
         pred_cls = prediction.argmax(axis=-1)
 
-        return render_template('Search.html')
+        if pred_cls == 0:
+            result = '63빌딩'
+        elif pred_cls == 1:
+            result = '남산타워'
+        elif pred_cls == 2:
+            result = '경복궁'
+        elif pred_cls == 3:
+            result = '광장시장'
+        elif pred_cls == 4:
+            result = 'four'
+        elif pred_cls == 5:
+            result = '뚝섬한강공원'
+        elif pred_cls == 6:
+            result = '롯데타워'
+        elif pred_cls == 7:
+            result = '봉은사'
+        elif pred_cls == 8:
+            result = '북촌한옥마을'
+        else:
+            result = '서울숲'
+
+        return render_template('Search.html', result=result)
     
     else:
         return render_template('image_upload.html')
